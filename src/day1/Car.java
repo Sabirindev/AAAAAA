dd1335fa76b39e7f14529f56b2d97433e718933c
SerifeElifAk <elifsubas@gmail.com>
2021-08-07 19:26:38 -0400

    My cats name is Alice

--- src/day1/Car.java
+++ src/day1/Car.java
@@ -1,7 +1,10 @@
 package day1;
 
 public class Car {
+
     public static void main(String[] args) {
         System.out.println("it is beginning");
+        System.out.println("Black cats are lovely");
     }
+
 }
