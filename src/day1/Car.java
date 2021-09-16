ecbe2c6fadbfbc652321be8cf44e0d68f5e3070d
KseniiaMazanko <ksmazanko@gmail.com>
2021-09-15 20:33:56 -0400

    New changes

--- src/day1/Car.java
+++ src/day1/Car.java
@@ -4,6 +4,11 @@ public class Car {
     public static void main(String[] args) {
         System.out.println("it is beginning");
         //this is a Kseniia's change
+
+
+        //new changes
+
+
     }
 
 }
